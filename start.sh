2ee7736919071ba21e92a6e40772ba6b31be5a78
PaulChen <33018020+PaulCCCCCCH@users.noreply.github.com>
2019-11-14 14:14:03 +0000

    Full pipeline

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # Generate dataset of size 100000 using 3-grams, filter out words appeared less than 3 times
-# python generate.py 100000 3 --rebuild --fil 3 --outdir ./data
+python generate.py 100000 3 --rebuild --fil 3 --outdir ./data
 
 # Train a keyword model using mlp
 python main.py reg_attention keyword_model_entropy_reg_attention \
@@ -31,3 +31,4 @@ python main.py baseline_lstm additive_lstm_sparse_attention \
         --kwm_path ./models/keyword_model_sparse_attention \
         --epoch 20 \
         --learning_rate 1.0
+       
